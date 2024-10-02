56b483874d36b0e0284e48b5e44b35ed5a9215c6
Ricardoncio <rsolo719@gmail.com>
2024-10-02 14:25:45 +0200

    offcanvas, Footer y empezamos main

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -1,1 +1,13 @@
 console.log("Hola Mundo!")
+
+document.querySelector("input#lenguaje").value = navigator.language;
+
+document.querySelectorAll(".open-offcanvas").forEach((e) => {
+    e.addEventListener('click', () => {
+        document.querySelector("aside#offcanvas").classList.add("show");
+    });
+});
+
+document.querySelector("button.close").addEventListener('click', () => {
+    document.querySelector("aside#offcanvas").classList.remove("show");
+});
