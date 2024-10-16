98b28663503e2dfcac1c9921b1651645cda7baa6
Ricardoncio <rsolo719@gmail.com>
2024-10-16 14:18:34 +0200

    otro achuchon

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -5,9 +5,19 @@ document.querySelector("input#lenguaje").value = navigator.language;
 document.querySelectorAll(".open-offcanvas").forEach((e) => {
     e.addEventListener('click', () => {
         document.querySelector("aside#offcanvas").classList.add("show");
-    });
-});
+    })
+})
 
 document.querySelector("button.close").addEventListener('click', () => {
     document.querySelector("aside#offcanvas").classList.remove("show");
-});
+})
+
+document.querySelectorAll("#galeria .inner figure").forEach((e) => {
+    e.addEventListener('click', (event) => {
+        const dialog = document.querySelector("dialog#lightbox");
+        dialog.querySelector("img").src = e.querySelector("img").src;
+        dialog.showModal();
+
+    })
+
+})
