01c844eeaeeecb9fd728e97a0c9b6ae8758e1ce6
Ricardoncio <rsolo719@gmail.com>
2024-10-21 09:06:16 +0200

    Pasar html a ejs

--- web/routes/index.js
+++ web/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let h1Text = "Restaurante Viva María - El Palo";
-  res.render("home", {title: h1Text});
+  res.render("home", {title: "Principal"});
 });
 router.get('/celebraciones', function(req, res, next) {
   res.redirect("/celebraciones.html");
@@ -13,7 +13,7 @@ router.get('/galeria', function(req, res, next) {
   res.redirect("/galeria.html");
 });
 router.get('/contacto', function(req, res, next) {
-  res.redirect("/contacto.html");
+  res.render("contacto", {title: "Contacto"});
 });
 
 
